88294a7149c7c19f6e48cdf3b2428b26f9163c20
A.J. Bale <abalejr@gmail.com>
2018-06-06 17:10:27 -0500

    Add js from original videos to reference file

---
+++ 6.6.18_original_archive_videos.js
@@ -0,0 +1,84 @@
+//VIDEO 1
+
+document.querySelector('div.archive-video-player').id = '12';
+
+jwplayer("12").setup({
+	"file": "https://cloud-streaming.s3.amazonaws.com/chatrecordings/SFGold/1008-Raghee%2520Horner-06-06-2018__08.06.282_AM.mp4",
+	"image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-rh.jpg",
+	playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+	width: "100%",
+	aspectratio: "16:9"
+});
+
+//VIDEO 2
+
+document.querySelector('div.archive-video-player').id = '15';
+
+jwplayer("15").setup({
+    "file": "https://cloud-streaming.s3.amazonaws.com/chatrecordings/SFGold/1008-SO_Neil-06-06-2018__09.23.059_AM.mp4",
+    "image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-ny.jpg",
+    playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+    width: "100%",
+    aspectratio: "16:9"
+});
+
+//VIDEO 3
+
+document.querySelector('div.archive-video-player').id = '10';
+
+jwplayer("10").setup({
+    "file": "https://cloud-streaming.s3.amazonaws.com/chatrecordings/SFGold/1008-David%2520Starr-06-06-2018__11.36.311_AM.mp4",
+    "image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-ds.jpg",
+    playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+    width: "100%",
+    aspectratio: "16:9"
+});
+
+//VIDEO 4
+
+document.querySelector('div.archive-video-player').id = '21';
+
+jwplayer("21").setup({
+    "file": "https://cloud-streaming.s3.amazonaws.com/chatrecordings/SFGold/1008-ST_JaredD-06-06-2018__01.35.579_PM.mp4",
+    "image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-jdlc.jpg",
+    playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+    width: "100%",
+    aspectratio: "16:9"
+});
+
+//VIDEO 5
+
+document.querySelector('div.archive-video-player').id = '2';
+
+jwplayer("2").setup({
+    "file": "https://cloud-streaming.s3.amazonaws.com/chatrecordings/SFGold/%2B1008-Raghee%2520Horner-06-06-2018__02.03_PM_edit.mp4",
+    "image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-rh.jpg",
+    playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+    width: "100%",
+    aspectratio: "16:9"
+});
+
+//VIDEO 6
+
+document.querySelector('div.archive-video-player').id = '17';
+
+jwplayer("17").setup({
+    "file": "https://cloud-streaming.s3.amazonaws.com/chatrecordings/SFGold/1008-Raghee%2520Horner-06-06-2018__04.16.532_PM.mp4",
+    "image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-rh.jpg",
+    playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+    width: "100%",
+    aspectratio: "16:9"
+});
+
+//VIDEO 7
+
+
+document.querySelector('div.archive-video-player').id = '22';
+
+jwplayer("22").setup({
+    "file": "https://simpler-crypto.s3.amazonaws.com/2018/05_21_18%20-%20nightly.mov",
+    "image": "https://s3.amazonaws.com/simpler-cdn/azure-blob-files/chatroom/chatroom-blank.jpg",
+    playbackRateControls: [0.75, 1, 1.25, 1.5, 2.0],
+    width: "100%",
+    aspectratio: "16:9"
+});
